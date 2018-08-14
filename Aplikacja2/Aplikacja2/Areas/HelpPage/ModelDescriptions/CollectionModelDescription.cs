f1d43ce4aa9893b8d93171ac836f60bfd9240520
Lucjan Moczulski <lucjan.moczulski@totalizator.pl>
2018-08-14 10:54:40 +0200

    sgsdfgsdfg

---
+++ Aplikacja2/Aplikacja2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Aplikacja2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Aplikacja2/Aplikacja2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aplikacja2.WebApiApplication" Language="C#" %>
